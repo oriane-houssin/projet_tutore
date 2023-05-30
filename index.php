0ffb2e52fda5a66d84d0b53bc8d2747c3f9ac5be
oriane-houssin <oriane.houssin@next-u.fr>
2023-05-30 13:59:56 +0200

    n

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+require_once('connection.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    
+</body>
+</html>
